c377af71cc0373dd42571b0291babfbc1a228c2a
Sebastian Steinhauer <s.steinhauer@yahoo.de>
2020-02-19 11:00:34 +0100

    Find player

--- player.py
+++ player.py
@@ -4,18 +4,27 @@ import random
 
 class Player:
     VERSION = "bet 4"
+    PLAYER_NAME = 'Player One'
 
     def __init__(self, game_state):
         self.game_state = game_state
 
+    def log(self, message):
+        sys.stderr.writelines('**********************')
+        sys.stderr.writelines(message)
+
     def betRequest(self):
-        sys.stderr.writelines(self.game_state)
+        self.log(self.game_state)
+        self.log(self.get_our_player())
         return 4
 
     def showdown(self):
-        sys.stderr.writelines("player one")
-        sys.stderr.writelines(self.game_state)
+        self.log('showdown')
+        self.log(self.game_state)
 
     def get_our_player(self):
         """ Find our player """
-        pass
+        for player in self.game_state['players']:
+            if player['name'] == self.PLAYER_NAME:
+                return player
+        return None
