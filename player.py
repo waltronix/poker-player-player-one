ecc48b9333bcc13906de41bc2566776b2e512fb8
Sebastian Steinhauer <s.steinhauer@yahoo.de>
2020-02-19 13:54:52 +0100

    community cards

--- player.py
+++ player.py
@@ -138,11 +138,9 @@ class Player:
         amount = self.game_state['current_buy_in'] - player['bet']
         score = hand.get_hand_score()
 
-        """
         if self.game_state.community_cards:
             card_score = hand.get_full_score()
             score = min(score, card_score)
-        """
 
         if score == 1:
             amount += self.game_state['minimum_raise'] * 10
