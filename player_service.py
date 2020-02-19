57b9a8af5d2348467a0fac4d2567174a6e785b2b
Sebastian Steinhauer <s.steinhauer@yahoo.de>
2020-02-19 10:48:25 +0100

    Go back to fucking Python 2

--- player_service.py
+++ player_service.py
@@ -1,36 +1,57 @@
-import os
 import time
+import cgi
 import json
-from bottle import route, run, request
+import BaseHTTPServer
+import os
 from player import Player
 
 
-HOST_NAME = "0.0.0.0"
-PORT_NUMBER = int(os.environ.get("PORT", 0)) or 9000
+HOST_NAME = '0.0.0.0'
+PORT_NUMBER = os.environ.has_key('PORT') and int(os.environ['PORT']) or 9000
 
 
-@route('/', method='POST')
-def handler():
-    action = request.forms.get('action')
-    if action == 'check':
-        return 'check'
-    elif action == 'version':
-        return Player.VERSION
-    elif action == 'bet_request':
-        state = json.loads(request.forms.get('game_state', '{}'))
-        return str(Player().betRequest(state))
-    elif action == 'showdown':
-        state = json.loads(request.forms.get('game_state', '{}'))
-        Player().showdown(state)
-        return ''
-    else:
-        return 'Fuck you!'
+class PlayerService(BaseHTTPServer.BaseHTTPRequestHandler):
 
+    def do_POST(self):
+
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+
+        ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+        if ctype == 'multipart/form-data':
+            postvars = cgi.parse_multipart(self.rfile, pdict)
+        elif ctype == 'application/x-www-form-urlencoded':
+            length = int(self.headers.getheader('content-length'))
+            postvars = cgi.parse_qs(self.rfile.read(length), keep_blank_values=1)
+        else:
+            postvars = {}
+
+        action = postvars['action'][0]
+
+        if 'game_state' in postvars:
+            game_state = json.loads(postvars['game_state'][0])
+        else:
+            game_state = {}
+
+
+        response = ''
+        if action == 'bet_request':
+            response = Player().betRequest(game_state)
+        elif action == 'showdown':
+            Player().showdown(game_state)
+        elif action == 'version':
+            response = Player.VERSION
+
+        self.wfile.write(response)
 
 if __name__ == '__main__':
+    server_class = BaseHTTPServer.HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), PlayerService)
     print(time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER))
     try:
-        run(host=HOST_NAME, port=PORT_NUMBER, debug=True)
+        httpd.serve_forever()
     except KeyboardInterrupt:
         pass
+    httpd.server_close()
     print(time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER))
